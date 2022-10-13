cccbf210a39f56ce8736ce83f344b6908778a93e
Ricardo Dias <ricardodc@gmail.com>
2022-10-13 03:25:47 -0300

    Change from vim to lua and adding a custom bar

--- lua/user/plugin.lua
+++ lua/user/plugin.lua
@@ -1,4 +1,14 @@
-return require('packer').startup(function(use)
+local status, packer = pcall(require, "packer")
+
+if (not status) then
+    print("Packer is not installed")
+    return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
   use { 'wbthomason/packer.nvim' }
   use { 'dracula/vim', as 'dracula' }
+
 end)
