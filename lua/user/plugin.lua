9daecb54d771348ca91b6ccfeb47175f6d6793ef
Ricardo Cavalcante <ricardodc@gmail.com>
2022-10-15 02:43:05 -0300

    Change tab and references

--- lua/user/plugin.lua
+++ lua/user/plugin.lua
@@ -9,6 +9,6 @@ vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
   use { 'wbthomason/packer.nvim' }
-  use { 'dracula/vim', as 'dracula' }
+  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
 
 end)
