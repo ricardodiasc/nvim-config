815f1d7ca0e4f6265b9e248bf655fe84eb369117
Ricardo Dias <ricardodc@gmail.com>
2022-10-23 00:00:12 -0300

    Renaming plugin.lua

--- lua/user/plugin.lua
+++ lua/user/plugin.lua
@@ -5,10 +5,13 @@ if (not status) then
     return
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd [[ packadd packer.nvim ]]
 
 packer.startup(function(use)
   use { 'wbthomason/packer.nvim' }
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-
+  use { 'nvim-lualine/lualine.nvim', requires = 'kyazdani42/nvim-web-devicons'} --Statusline
+  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'neovim/nvim-lspconfig'
 end)
